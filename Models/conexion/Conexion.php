9fe229aa7c699b6ee047ac5902c73575896ee2d7
Your Name <laura2003ramirez@gmail.com>
2022-03-20 00:35:50 -0500

    intento heroku + postgre configuracion

--- Models/conexion/Conexion.php
+++ Models/conexion/Conexion.php
@@ -2,11 +2,11 @@
 
 class Database
 {
-    private static $server = 'localhost;port=3310';
+    private static $server = 'ec2-44-192-245-97.compute-1.amazonaws.com;port=5432';
 
-    private static $dbName = 'dailylab';
-    private static $dbUser = 'Proyecto';
-    private static $dbPass = '2184573Dailylab'; 
+    private static $dbName = 'd1a7mom45t35mo';
+    private static $dbUser = 'ujdhgrrkvsismt';
+    private static $dbPass = 'f5e41db87aef3e018ac0b761e12c0ff2327507fdd86de69c26f007f961e26e0d'; 
 
     public static function Connect()
     {
